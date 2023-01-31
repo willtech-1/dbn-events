4ddd2ef5c43d376be67623b4038f92ba097ef47b
Aphile <aphilentando55@gmail.com>
2023-01-31 19:56:33 +0200

    Updated Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-white dark:bg-gray-900">
       <div className="py-6 px-4 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-          © 2022 <a href="https://flowbite.com/">OurEvents™</a>. All Rights
+          © 2023 <a href="https://flowbite.com/">OurEvents™</a>. All Rights
           Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0">
